38dcde0a51aa08376c76d39045a5980a051cceb8
23160rs <23160rs@hvhs.school.nz>
2025-05-02 02:34:33 +0000

    May 2, 2025, 2:34 AM

--- t01_introduction.js
+++ t01_introduction.js
@@ -1,4 +1,8 @@
 
+/**********************************
+Task 1
+Rhys
+Date 1/05/2025
+**********************************/
 
 
-console.log("Rhys scott 29/4/2025" I made this to see my coding skills )
