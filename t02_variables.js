8e512a37395c7abcdc9fdd3ab11d1bce05689fa3
23160rs <23160rs@hvhs.school.nz>
2025-05-08 01:30:04 +0000

    s

--- t02_variables.js
+++ t02_variables.js
@@ -2,7 +2,7 @@
 Task 2 variables 
 Rhys
 Date 1/05/2025
-/*********************************/
+*********************************/
 
 console.log( "Running T1-variabels.js" );
 
@@ -10,12 +10,12 @@ console.log( "Running T1-variabels.js" );
 
 let UserMoney =10;
 let poketMoney =10;
-let MyMoney =10;
-let Username=Rhys Scott;
-/************/
-Maincode
-/**********************************/
+let Username="Rhys Scott";
+let Date=2025
 
-
-
-/**********************************/
+/**********************************
+//Maincode
+**********************************/
+console.log()
+console.log
+/**********************************
