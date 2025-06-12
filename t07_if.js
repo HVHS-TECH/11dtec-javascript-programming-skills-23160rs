be0d5e3c95222d20f9b47c484e530eb8d45441c5
23160rs <23160rs@hvhs.school.nz>
2025-06-12 00:55:07 +0000

    Jun 12, 2025, 12:55 AM

---
+++ t07_if.js
@@ -0,0 +1,34 @@
+/**********************************
+task 6 introduction to arrays 
+********************************/
+
+console.log( "Running T5-alerts_prompts.js" );
+
+//varables
+let username = "Rhys scott"
+let userage = "15"
+let pocketmoney ="10"
+let year = "2025"
+let cars = ["Toyota","Ford","Honda" ];
+
+
+
+
+/**********************************
+
+maincode
+
+
+
+
+**********************************/
+prompt("on a scale of 0 to 3 how much do you like cars");
+
+prompt("what is your favourite car brand out of Toyota, Ford, Honda")
+
+
+
+
+/***********************************
+ Functions
+ **********************************/
