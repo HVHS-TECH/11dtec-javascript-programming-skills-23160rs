1dad59b5122f85e6a39586f1b8e2417db54ff265
23160rs <23160rs@hvhs.school.nz>
2025-06-16 21:50:41 +0000

    Jun 16, 2025, 9:50 PM

--- t07_if.js
+++ t07_if.js
@@ -25,8 +25,8 @@ alert("Hi")
 userage = prompt("how old are you")
 alert("I predict that you were born in " + (year - userage))
 pocketmoney = prompt("How much money do you have")
-prompt("you have " + pocketMoney)
-if (pocketmoney >= 10) {
+prompt("you have " + pocketmoney)
+if (pocketmoney <= 10) {
 alert("you CAN NOT afford a chocolate bar")
 } else {
 alert("you CAN afford a chocolate bar")
---
+++ t08_for_loop.js
@@ -0,0 +1,25 @@
+
+/**********************************
+task 1 introduction to loops
+********************************/
+
+console.log( "Running T8-for-loop.js" );
+
+//varables
+
+/**********************************
+maincode
+**********************************/
+
+
+//Loop from 0 to 9
+console.log("starting loop");
+for (i = 0; i < 9; i ++) {
+  console.log("current count: " + i);
+}
+
+console.log("Loop stopped!")
+
+/***********************************
+ Functions
+ **********************************/
